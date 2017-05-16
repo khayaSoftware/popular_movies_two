c51c20989c03df4ce0ca1f5c93960f43d3c657e7
khaya <khaya.khumalo93@gmail.com>
2017-05-16 12:15:50 +0100

    Adding recyclerview

---
+++ app/src/main/java/com/example/android/app/khayapopularmovies/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.android.app.khayapopularmovies;
+
+import android.support.v7.app.AlertController;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mMovieList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mMovieList = (RecyclerView) findViewById(R.id.rv_movies);
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/android/app/khayapopularmovies/MovieAdapter.java
@@ -0,0 +1,18 @@
+package com.example.android.app.khayapopularmovies;
+
+import android.support.v7.widget.RecyclerView;
+
+/**
+ * Created by User on 5/16/2017.
+ */
+
+public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
+
+    private static final String TAG = MovieAdapter.class.getSimpleName();
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder{
+
+
+    }
+
+}
