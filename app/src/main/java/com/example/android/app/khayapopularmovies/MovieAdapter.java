9812eaec5d752cce6a4c81863f24c8a3975a78c0
AlanCowap <spam@alancowap.com>
2017-06-20 13:03:19 +0100

    Third review 20-06-2017.

--- app/src/main/java/com/example/android/app/khayapopularmovies/MovieAdapter.java
+++ app/src/main/java/com/example/android/app/khayapopularmovies/MovieAdapter.java
@@ -18,6 +18,7 @@ import static java.security.AccessController.getContext;
 /**
  * Created by User on 5/16/2017.
  */
+//TODO-2 SUGGESTION Be careful of templates, it could appear as though the work was not yours.
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.MovieViewHolder>{
 
